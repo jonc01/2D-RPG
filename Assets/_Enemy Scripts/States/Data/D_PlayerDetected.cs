fbde54cdda20ca7851a73da112fe6c3a8f020787
Jonathan Chen <chen.jonathanT@gmail.com>
2020-10-22 10:27:42 -0700

    Initial commit

--- Assets/_Enemy Scripts/States/Data/D_PlayerDetected.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "newPlayerDetectedStateData", menuName = "Data/State Data/Player Detected State")]
-public class D_PlayerDetected : ScriptableObject
-{
-    
-}
